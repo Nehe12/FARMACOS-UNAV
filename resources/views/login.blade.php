997aa9ef1146255ea27f3091e7a89e14731b3322
Nehe12 <101309955+Nehe12@users.noreply.github.com>
2023-05-14 00:30:25 -0700

    "Final logiin"

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,35 +16,43 @@
     </div>
     @endif
 </div>
-<div class="p-6">
-    <div class="row">
-        <div class="col image">
+
+<div class="container p-6 w-75 bg-light mt-5 rounded shadow ancho">
+    <div>
+        <h2 class="text-center">
+            BIENVENIDO
+        </h2>
+    </div>
+    <div class="row p-3">
+        <div class="col bg d-none d-lg-block">
+            <img src="/img/logoNutri.png" class="rounded mx-auto d-block" width="100%" alt="...">
 
         </div>
         <div class="col">
             <form action="{{route('sesion.login')}}" method="post">
                 @csrf
                 @method('POST')
-                <div class="col-md-7">
+                <div class="col-mb-5 p-4">
                     <label for="password" class="form-label">Password</label>
                     <input type="password" name="password" class="form-control" id="password">
                     @error('password') {{$message}}@enderror
                 </div>
-                <div class="col-md-7">
+                <div class="col-mb-4 p-4" >
                     <label for="email" class="form-label">Email address</label>
                     <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     @error('email') {{$message}}@enderror
                 </div>
 
-                <!-- <div class="mb-3 form-check">
-        <input type="checkbox" name="remember" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Recordar</label>
-    </div> -->
-                <button type="submit" class="btn btn-primary">Submit</button>
+
+                <div class="d-grid p-4">
+                    <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+                </div>
+
             </form>
         </div>
     </div>
 </div>
+
 
 @endsection
